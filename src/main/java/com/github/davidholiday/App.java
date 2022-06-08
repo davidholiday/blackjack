82a36eba9abf4bb674875879ec0d8c1ba90f4cbc
snerd <you@example.com>
2022-06-07 18:39:38 -0600

    adds multithreading to round count over 1k

--- src/main/java/com/github/davidholiday/App.java
+++ src/main/java/com/github/davidholiday/App.java
@@ -1,14 +1,9 @@
 package com.github.davidholiday;
 
 import com.github.davidholiday.agent.AgentPosition;
-import com.github.davidholiday.agent.Dealer;
 import com.github.davidholiday.agent.Player;
 import com.github.davidholiday.agent.strategy.count.NoCountStrategy;
 import com.github.davidholiday.agent.strategy.play.BasicFourSixEightDeckPlayerStrategy;
-import com.github.davidholiday.agent.strategy.play.DealerStrategy;
-import com.github.davidholiday.agent.strategy.play.NoOpPlayerStrategy;
-import com.github.davidholiday.agent.strategy.play.StandardDealerStrategy;
-import com.github.davidholiday.cardcollection.Shoe;
 import com.github.davidholiday.game.Game;
 import com.github.davidholiday.game.Rule;
 import com.github.davidholiday.game.RuleSet;
@@ -16,6 +11,10 @@ import com.github.davidholiday.util.RuntimeInfo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
 public class App {
@@ -25,31 +24,75 @@ public class App {
 
     public static void main( String[] args ) {
 
-        NoCountStrategy noCountStrategy = new NoCountStrategy();
-        //NoOpPlayerStrategy noOpPlayerStrategy = new NoOpPlayerStrategy();
-        BasicFourSixEightDeckPlayerStrategy playerStrategy = new BasicFourSixEightDeckPlayerStrategy();
-        Player playerOne = new Player(noCountStrategy, playerStrategy, 10);
-        Player playerTwo = new Player(noCountStrategy, playerStrategy, 10);
+        List<Game> gameList = getGameList();
 
-        RuleSet ruleSet = new RuleSet.Builder()
-                                     .withRule(Rule.BLACKJACK_PAYS_THREE_TO_TWO)
-                                     .withRule(Rule.SIX_DECK_SHOE)
-                                     .withRule(Rule.PLAYER_CAN_DOUBLE_ON_ANY_FIRST_TWO_CARDS)
-                                     .withRule(Rule.PLAYER_CAN_RESPLIT_TO_FOUR_HANDS)
-                                     .withRule(Rule.PLAYER_CAN_LATE_SURRENDER)
-                                     .build();
+        int numRounds = 1;
+        if (args.length == 1) {
+            try {
+                numRounds = Integer.parseInt(args[0]);
+                if (numRounds < 1) { throw new NumberFormatException();}
+            } catch (NumberFormatException e) {
+                LOG.error("numRounds argument must be a positive number greater than zero!");
+                System.exit(8);
+            }
+        }
 
-        Game game = new Game.Builder()
-                            .withPlayerAtPosition(playerOne, AgentPosition.PLAYER_ONE)
-                            .withPlayerAtPosition(playerTwo, AgentPosition.PLAYER_TWO)
-                            .withRuleSet(ruleSet)
-                            .build();
+        int numWorkers = RUNTIME_INFO.AVAILABLE_PROCESSORS;
+        if (numRounds < 1001) {
+            LOG.info("running in single thread for: {} rounds", numRounds);
+            gameList.get(0).playRounds(numRounds);
+        } else {
+            LOG.info("will spin up: {} threads for: rounds", numRounds);
+            int roundsPerWorker = numRounds / numWorkers;
+            int roundsPerWorkerRemainder = numRounds % numWorkers;
+            LOG.info("{} rounds will be handled per thread", roundsPerWorker);
+            if (roundsPerWorkerRemainder > 0) {
+                LOG.info("handling remainder of {} rounds in single thread first", roundsPerWorkerRemainder);
+                gameList.get(0).playRounds(roundsPerWorkerRemainder);
+                LOG.info("done!");
+            }
+            LOG.info("starting workers...");
+            gameList.parallelStream().forEach(g -> g.playRounds(roundsPerWorker));
+            LOG.info("done!");
+        }
 
-        /*
-         TODO make a parallel stream of these and activate them in parallel
-         */
-        game.playRounds(1);
+        //game.playRounds(numRounds);
 
+    }
+
+    /**
+     * doing it this way for now because a copy constructor would involve a lot of deep copy work that I
+     * don't want to do right now...
+     * @return
+     */
+    private static List<Game> getGameList() {
+        List<Game> gameList = new ArrayList<>();
+
+        for (int i = 0; i < RUNTIME_INFO.AVAILABLE_PROCESSORS; i ++)  {
+            NoCountStrategy noCountStrategy = new NoCountStrategy();
+            //NoOpPlayerStrategy noOpPlayerStrategy = new NoOpPlayerStrategy();
+            BasicFourSixEightDeckPlayerStrategy playerStrategy = new BasicFourSixEightDeckPlayerStrategy();
+            Player playerOne = new Player(noCountStrategy, playerStrategy, 10);
+            Player playerTwo = new Player(noCountStrategy, playerStrategy, 10);
+
+            RuleSet ruleSet = new RuleSet.Builder()
+                                         .withRule(Rule.BLACKJACK_PAYS_THREE_TO_TWO)
+                                         .withRule(Rule.SIX_DECK_SHOE)
+                                         .withRule(Rule.PLAYER_CAN_DOUBLE_ON_ANY_FIRST_TWO_CARDS)
+                                         .withRule(Rule.PLAYER_CAN_RESPLIT_TO_FOUR_HANDS)
+                                         .withRule(Rule.PLAYER_CAN_LATE_SURRENDER)
+                                         .build();
+
+            Game game = new Game.Builder()
+                                .withPlayerAtPosition(playerOne, AgentPosition.PLAYER_ONE)
+                                .withPlayerAtPosition(playerTwo, AgentPosition.PLAYER_TWO)
+                                .withRuleSet(ruleSet)
+                                .build();
+
+            gameList.add(game);
+        }
+
+        return gameList;
     }
 
 }
--- src/main/java/com/github/davidholiday/util/RuntimeInfo.java
+++ src/main/java/com/github/davidholiday/util/RuntimeInfo.java
@@ -7,9 +7,14 @@ public class RuntimeInfo {
 
     public final boolean ASSERTIONS_ENABLED;
 
+    public final int AVAILABLE_PROCESSORS;
+
     public RuntimeInfo() {
         // t/y SO https://stackoverflow.com/a/16788186
         this.ASSERTIONS_ENABLED = ManagementFactory.getRuntimeMXBean().getInputArguments().contains("-ea");
+
+        // t/y SO https://stackoverflow.com/a/16128493
+        this.AVAILABLE_PROCESSORS = Runtime.getRuntime().availableProcessors();
     }
 
 }
